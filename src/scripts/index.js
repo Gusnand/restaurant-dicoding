4b6d26175fdc5344c621cdb555586f4fa86da1f3
Gusnand <gusnanda1170@gmail.com>
2023-11-30 06:46:28 +0800

    resto card added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,12 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButtonElement = document.querySelector("#menu-toggle");
+  const drawerElement = document.querySelector("#drawer");
+
+  hamburgerButtonElement.addEventListener("click", (event) => {
+    drawerElement.classList.toggle("open");
+    event.stopPropagation();
+  });
+});
