5650801c5adda117ea44547d24e2c26450b6eb6a
Gusnand <gusnanda1170@gmail.com>
2023-11-30 12:43:40 +0800

    final adjustment from reviewer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,7 @@ import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const hamburgerButtonElement = document.querySelector("#menu-toggle");
+  const hamburgerButtonElement = document.querySelector("nav button");
   const drawerElement = document.querySelector("#drawer");
 
   hamburgerButtonElement.addEventListener("click", (event) => {
