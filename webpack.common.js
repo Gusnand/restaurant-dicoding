d222527c294d2206f6b9f598dfe1af8e144deb18
Gusnand <gusnanda1170@gmail.com>
2023-11-30 10:09:05 +0800

    final adjustment again

--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
